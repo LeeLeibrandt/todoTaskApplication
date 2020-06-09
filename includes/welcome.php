d41bf68efa79b514a41bc9356791d1aa7d48e202
Lee Leibrandt <60574726+LeeLeibrandt@users.noreply.github.com>
2020-06-09 14:34:50 +0200

    Update welcome.php

--- includes/welcome.php
+++ includes/welcome.php
@@ -32,7 +32,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. What should we accomplish first.</h1>
         </div>
         <p>
-            <a href="../app/index.php" class="btn box ">Todo list</a>
+            <a href="../application/index.php" class="btn box">App</a>
             <a href="../calendar/index.php" class="btn outline ">My Calendar</a>
             <a href="reset.php" class="btn box ">Reset Pass</a>
             <a href="logout.php" class="btn outline">Sign Out</a>
@@ -41,11 +41,11 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <div class="img">
                 <img src="../img/1.png" alt="">
             </div>
-        <div class="curve end">
+        <!--<div class="curve end">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
                 <path fill="rgb(193, 144, 240)" fill-opacity="0.5" d="M0,192L48,181.3C96,171,192,149,288,144C384,139,480,149,576,170.7C672,192,768,224,864,245.3C960,267,1056,277,1152,245.3C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
             </svg>
-        </div>
+        </div>-->
     </div>
 </body>
 </html>
